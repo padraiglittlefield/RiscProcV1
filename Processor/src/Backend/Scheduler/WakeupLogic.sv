0d5b23d1bca7d409e91e6c8f9f46426177532161
padraiglittlefield <padraiglittlefield@gmail.com>
2025-06-06 16:32:08 -0400

    Wakeup

--- Processor/src/Backend/Scheduler/WakeupLogic.sv
+++ Processor/src/Backend/Scheduler/WakeupLogic.sv
@@ -35,16 +35,17 @@ logic [7:0] entry_latency_in [0:NUM_FUS-1];
     src1_dp_loc: {7:4->FU_INDEX, 3:0->COL_INDEX}
     Set Lines [(src1_dp_loc[7:4]<< $clog2(NUM_FUS)) + src1_dp_loc[3:0]] = 1;
 */
+
 always @(posedge clk) begin
     if(rst) begin
         set_lines <= '0;
     end else begin
         set_lines <= '0;
         if(wakeupDispatch.src1_dp_en) begin
-            set_lines[(src1_dp_loc[7:4]<< $clog2(NUM_FUS)) + src1_dp_loc[3:0]] <= 1'b1;
+            set_lines[src1_dp_loc[($clog2(NUM_FUS) + $clog2(NUM_COLS))-1 : $clog2(NUM_COLS)]<< $clog2(NUM_COLS) + src1_dp_loc[$clog2(NUM_COLS)-1:0]] <= 1'b1;
         end
         if(wakeupDispatch.src2_dp_en) begin
-            set_lines[(src2_dp_loc[7:4]<< $clog2(NUM_FUS)) + src2_dp_loc[3:0]] <= 1'b1;
+            set_lines[src2_dp_loc[($clog2(NUM_FUS) + $clog2(NUM_COLS))-1 : $clog2(NUM_COLS)]<< $clog2(NUM_COLS) + src2_dp_loc[$clog2(NUM_COLS)-1:0]] <= 1'b1;
         end
     end
 end
@@ -53,18 +54,18 @@ assign w_en = dispatch_valid & entry_free;  // Only write when both the dispatch
 
 genvar i;
 generate
-    for(i = 1; i <= NUM_FUS; i++) begin
+    for(i = 0; i < NUM_FUS; i++) begin
         DependencyMatrix matrix (
             .clk(clk),
             .rst(rst),
             .w_en(w_en),                       
             .w_row_index(w_row_index),       
-            .set_lines(set_lines[(NUM_COLS*i)-1:(NUM_COLS*(i-1))]), 
+            .set_lines(set_lines[(NUM_COLS*(i+1))-1:(NUM_COLS*i)]), 
             .clear_en(clear_en),      
             .clear_lines(clear_lines),                
             .free_en(free_en),       
             .free_row_index(free_row_index),     
-            .ready_vector(ready_vector[(NUM_ROWS*i)-1:(NUM_ROWS*(i-1))]),
+            .ready_vector(ready_vector[(NUM_ROWS*(i+1))-1:(NUM_ROWS*(i))]),
 
         );
     end
@@ -92,20 +93,32 @@ FIFO #(
 
 logic entry_ready [(NUM_ROWS-1):0];
 
+
+/* Speculatve Instruction Wakeup
+    ==============================
+    - On the selection of an instruction, it will clear its entry line
+    - When an instruction has both srcs "ready", it will enable the shift register
+    - The shift register is loaded on the allocation of a entry with the latency of its dependency
+        - TODO: Fix later:
+            1. How do we know which latency was related to the most recently selected instruction
+            2. How to handle replay logic on the selection of Loads as their latency is not guarenteed
+*/
 genvar j;
-generate`
-    for(j = 1; j<= NUM_ROWS; j++) begin
-        
+generate
+    for(j = 0; j < NUM_ROWS; j++) begin
         ShiftRegister delayed_request(
             .clk(clk),
             .rst(rst),
-            .shift_en(entry_ready),    // Match
-            .w_en(),        // On select Rows  
-            .latency_in(),  // entry_latency_in -> Written to when an instruction is allocated
-            .ready(request_vector[(j-1)])        // 
+            .shift_en(entry_ready[j]),    // Match
+            .w_en(),          
+            .latency_in(),  
+            .ready(request_vector[j])        // 
         );
-
-        assign entry_ready = {ready_vector[(j-1)+0],ready_vector[(j-1)+1],ready_vector[(j-1)+2]ready_vector[(j-1)+3]} == '0 // Must edit if we increase number of FUs
+        genvar f;
+        for (f = 0; f < NUM_FUS; f++) begin
+            assign row_ready_bits[f] = ready_vector[f*NUM_ROWS + j];
+        end
+        assign entry_ready[j] = (row_ready_bits == '0);
     end
 
 endgenerate
--- Processor/src/interfaces/WakeupDispatchIF.svh
+++ Processor/src/interfaces/WakeupDispatchIF.svh
@@ -7,12 +7,15 @@ import CORE_PKG::*;
 
 interface WakeupDispatchIF;
 
+    localparam FU_IDX_WIDTH  = $clog2(NUM_FUS);
+    localparam COL_IDX_WIDTH = $clog2(NUM_COLS);
+    
     logic entry_free;       // Whether or not there is room in the entry queue for another instructions
     logic dispatch_valid;   // Whether or not dispatch instruction is valid
     logic src1_dp_en;      // Is src 1 waiting on an instruction to finish
     logic src2_dp_en;      // Is src 2 waiting on an instruction to finish
-    logic [(2 * NUM_FUS)-1:0] src1_dp_loc;     // Which srcs are not ready. Entry nums will be read from DLT or will be 0 if dst is ready
-    logic [(2 * NUM_FUS)-1:0] src2_dp_loc;     // Which srcs are not ready. Entry nums will be read from DLT
+    logic [(FU_IDX_WIDTH + COL_IDX_WIDTH)-1:0] src1_dp_loc;     // Which srcs are not ready. Entry nums will be read from DLT or will be 0 if dst is ready
+    logic [(FU_IDX_WIDTH + COL_IDX_WIDTH)-1:0] src2_dp_loc;     // Which srcs are not ready. Entry nums will be read from DLT
 
     modport Dispatch (
       input entry_free,
